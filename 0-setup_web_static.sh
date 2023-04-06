0084f3302a827f705cf6a595e6c8f25d8ed64815
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-06 12:14:58 +0300

    add task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#sets up your web servers for the deployment of web_static.
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+touch /data/web_static/releases/test/index.html
+echo '
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+' > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+NGINX_CONFIG="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+sed -i '/server_name _;/a'"$NGINX_CONFIG" /etc/nginx/sites-available/default
+service nginx restart
+service nginx reload
