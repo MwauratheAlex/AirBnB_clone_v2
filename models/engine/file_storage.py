6927dcdc18c4965f374cea852f02087cd8abb504
MwauratheAlex <mwaurathealex@gmail.com>
2023-04-26 10:18:36 +0300

    Update FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        temp = {}
+        for key, value in self.__objects.items():
+            if key.split(".")[0] == cls.__name__:
+                temp[key] = value
+        return temp
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.to_dict()['__class__'], obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
